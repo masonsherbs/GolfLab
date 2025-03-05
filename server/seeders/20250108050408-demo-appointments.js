b2db18b87787e92f5e9a023549d75ffbf6bce144
masonsherbs <masonsherburne@gmail.com>
2025-03-04 20:49:25 -0700

    Migrated to ES from CommonJS in Seeders

--- server/seeders/20250108050408-demo-appointments.js
+++ server/seeders/20250108050408-demo-appointments.js
@@ -1,28 +1,28 @@
 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Appointments', [
-      {
-        userId: 1,
-        dateTime: new Date(new Date().setDate(new Date().getDate() + 7)),
-        duration: 60,
-        status: 'scheduled',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        userId: 2,
-        dateTime: new Date(new Date().setDate(new Date().getDate() + 14)),
-        duration: 90,
-        status: 'scheduled',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ], {});
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Appointments', null, {});
-  }
+const up = async (queryInterface, Sequelize) => {
+  await queryInterface.bulkInsert('Appointments', [
+    {
+      userId: 1,
+      dateTime: new Date(new Date().setDate(new Date().getDate() + 7)),
+      duration: 60,
+      status: 'scheduled',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      userId: 2,
+      dateTime: new Date(new Date().setDate(new Date().getDate() + 14)),
+      duration: 90,
+      status: 'scheduled',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ], {});
 };
+
+const down = async (queryInterface, Sequelize) => {
+  await queryInterface.bulkDelete('Appointments', null, {});
+};
+
+export { up, down };
